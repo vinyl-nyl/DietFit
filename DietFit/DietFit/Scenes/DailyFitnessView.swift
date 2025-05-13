b2d2a060581d26c3e20d792c73d4be6a29557615
Heejung Yang <wolla39@gmail.com>
2025-05-13 09:35:08 +0900

    운동 탭 화면을 위한 DailyFitnessView 생성

---
+++ DietFit/DietFit/Scenes/DailyFitnessView.swift
@@ -0,0 +1,18 @@
+//
+//  DailyFitnessView.swift
+//  DietFit
+//
+//  Created by Heejung Yang on 5/13/25.
+//
+
+import SwiftUI
+
+struct DailyFitnessView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DailyFitnessView()
+}
