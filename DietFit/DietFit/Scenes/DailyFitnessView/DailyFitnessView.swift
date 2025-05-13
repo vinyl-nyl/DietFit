36b7d1d8d200a5a994300e11a0e1de2e623e6671
Heejung Yang <wolla39@gmail.com>
2025-05-13 16:57:56 +0900

    운동 종류 추가할 버튼 생성

--- DietFit/DietFit/Scenes/DailyFitnessView/DailyFitnessView.swift
+++ DietFit/DietFit/Scenes/DailyFitnessView/DailyFitnessView.swift
@@ -1,8 +1,41 @@
 //
 //  DailyFitnessView.swift
-//  DietFit
+//  Dietfit
 //
 //  Created by Heejung Yang on 5/13/25.
 //
 
-import Foundation
+import SwiftUI
+
+struct DailyFitnessView: View {
+    @Environment(\.colorScheme) private var colorScheme
+
+    @State var presentAddFitness: Bool = false
+
+    var body: some View {
+        Text("운동을 기록해주세요.")
+        VStack(alignment: .listRowSeparatorTrailing) {
+
+            Button {
+                presentAddFitness = true
+            } label: {
+
+                Image(systemName: "plus")
+                    .resizable()
+                    .frame(width: 15, height: 15)
+                    .tint(.white)
+                    .bold()
+                    .background(Color("ButtonPrimary"))
+                    .padding(15)
+                    .background(Color("ButtonPrimary"))
+                    .clipShape(Capsule())
+
+            }
+
+        }
+    }
+}
+
+#Preview {
+    DailyFitnessView()
+}
