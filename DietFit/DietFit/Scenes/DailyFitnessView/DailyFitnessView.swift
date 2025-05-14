97ad06c1383b575325c0c0829ed1b13ef55d58c6
Heejung Yang <wolla39@gmail.com>
2025-05-14 15:02:52 +0900

    운동 메인 페이지에 캘린더 추가

--- DietFit/DietFit/Scenes/DailyFitnessView/DailyFitnessView.swift
+++ DietFit/DietFit/Scenes/DailyFitnessView/DailyFitnessView.swift
@@ -10,27 +10,72 @@ import SwiftUI
 struct DailyFitnessView: View {
     @Environment(\.colorScheme) private var colorScheme
 
+    @State private var presentCalendar = false
     @State var presentAddFitness: Bool = false
+    @State private var selectDate = Date()
 
     var body: some View {
-        Text("운동을 기록해주세요.")
-        VStack(alignment: .listRowSeparatorTrailing) {
 
-            Button {
-                presentAddFitness = true
-            } label: {
+        VStack(alignment: .center) {
 
-                Image(systemName: "plus")
-                    .resizable()
-                    .frame(width: 15, height: 15)
-                    .tint(.white)
-                    .bold()
-                    .background(Color("ButtonPrimary"))
-                    .padding(15)
-                    .background(Color("ButtonPrimary"))
-                    .clipShape(Capsule())
+            HStack {
+                Button {
+                    presentCalendar = true
+                } label: {
+                    Text(dateFormat(selectDate))
+                        .font(.title3)
+                        .bold()
+                    Image(systemName: "arrowtriangle.down.fill")
+                }
+                .sheet(isPresented: $presentCalendar) {
+                    VStack {
+                        DatePicker("Select a date", selection: $selectDate, displayedComponents: [.date])
+                            .datePickerStyle(.graphical)
+                            .tint(Color.buttonPrimary)
+                            .padding()
+                            .onChange(of: selectDate) {
+                                presentCalendar = false
+                            }
+                    }
+                    .presentationDetents([.fraction(0.6)])
+                    .presentationDragIndicator(.visible)
+                }
+                .buttonStyle(.plain)
+                .padding(.horizontal)
 
             }
+            .frame(maxWidth: .infinity, alignment: .leading)
+            
+            ScrollCalendarView(selectDate: $selectDate)
+
+            VStack(spacing: 0) {
+                Text("운동을 기록해주세요.")
+                    .font(.title3)
+                    .bold()
+                    .frame(maxWidth: .infinity, alignment: .leading)
+
+
+                Button {
+                    presentAddFitness = true
+                } label: {
+
+                    Image(systemName: "plus")
+                        .resizable()
+                        .frame(width: 15, height: 15)
+                        .tint(.gray)
+                        .bold()
+                        .padding(15)
+                        .clipShape(Capsule())
+
+                }
+
+            }
+            .frame(width: 360, height: 360)
+            .modifier(CardStyleModifier())
+
+
+            Spacer()
+
         }
         .fullScreenCover(isPresented: $presentAddFitness, onDismiss: {
 
