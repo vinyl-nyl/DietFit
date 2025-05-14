c8269a35eb8422486a2529047bc1a598d0d7bf95
박동언 <dongeon8943@gmail.com>
2025-05-14 15:09:39 +0900

    서브뷰 분리 CardSummaryView 리팩토링

---
+++ DietFit/DietFit/Scenes/DailyMealView/SubViews/CardSummaryView.swift
@@ -0,0 +1,55 @@
+//
+//  CardSummaryView.swift
+//  DietFit
+//
+//  Created by 박동언 on 5/14/25.
+//
+
+import SwiftUI
+
+struct CardSummaryView: View {
+    @Environment(\.colorScheme) private var colorScheme
+
+    var body: some View {
+        VStack(spacing: 16) {
+            Text("오늘의 성과")
+                .font(.title3)
+                .bold()
+                .frame(maxWidth: .infinity, alignment: .leading)
+                .padding(.leading, 40)
+            VStack {
+                ZStack {
+                    Circle()
+                        .inset(by: -30)
+                        .rotation(.degrees(-180))
+                        .trim(from: 0, to: 0.5)
+                        .stroke(colorScheme == .dark ? Color(.systemGray4)  : Color(.systemGray6),
+                                style: .init(lineWidth: 40, lineCap: .round))
+                        .frame(width: 200, height: 200)
+                        .offset(y: 50)
+                    
+                    Circle()
+                        .inset(by: -30)
+                        .rotation(.degrees(-180))
+                        .trim(from: 0, to: 0.2)
+                        .stroke(Color.buttonPrimary, style: .init(lineWidth: 40, lineCap: .round))
+                        .frame(width: 200, height: 200)
+                        .offset(y: 50)
+                    //                                        .animation(.easeInOut, value: percentage)
+                    
+                    VStack {
+                        Text("500")
+                            .font(.title)
+                            .bold()
+                            .foregroundStyle(Color.buttonPrimary)
+                        Text("/ 2000 Kcal")
+                            .font(.title3)
+                    }
+                    .offset(y: 25)
+                }
+            }
+        }
+        .frame(width: 360, height: 280)
+        .modifier(CardStyleModifier())
+    }
+}
