66379dfb42ed685c1b7b3eb57630b515d4ad6e41
Heejung Yang <wolla39@gmail.com>
2025-05-14 13:56:54 +0900

    CategoryView 생성

---
+++ DietFit/DietFit/Scenes/DailyFitnessView/CategoryView.swift
@@ -0,0 +1,18 @@
+//
+//  CategoryView.swift
+//  DietFit
+//
+//  Created by Heejung Yang on 5/14/25.
+//
+
+import SwiftUI
+
+struct CategoryView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CategoryView()
+}
