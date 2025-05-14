fd7c75053c5c9fb663a81855274768c53a05c750
Heejung Yang <wolla39@gmail.com>
2025-05-14 14:07:39 +0900

    운동 종목 선택 시 운동 데이터 입력 뷰 생성

--- DietFit/DietFit/Scenes/DailyFitnessView/FitnessComposeView.swift
+++ DietFit/DietFit/Scenes/DailyFitnessView/FitnessComposeView.swift
@@ -1,6 +1,6 @@
 //
 //  FitnessComposeView.swift
-//  DietFit
+//  Dietfit
 //
 //  Created by Heejung Yang on 5/13/25.
 //
@@ -8,8 +8,82 @@
 import SwiftUI
 
 struct FitnessComposeView: View {
-    
+    @Environment(\.dismiss) var dismiss
+    @State private var isPresented: Bool = false
+
+    @State var duration: Int = 30
     var body: some View {
+        List {
+            Section() {
+                Text("109kcal 태웠어요")
+            } header: {
+                Text("Header")
+            }
+
+            Section() {
+                Stepper("\(duration) 분") {
+                    duration+=10
+                } onDecrement: {
+                    duration-=10
+                }
+
+            } header: {
+                Text("운동시간")
+            }
+
+            Section() {
+
+                HStack {
+                    Button {
+                        dismiss()
+                    } label: {
+                        Text("가볍게")
+                            .padding()
+    //                        .frame(maxWidth: .infinity)
+                            .background(.green)
+                            .clipShape(RoundedRectangle(cornerRadius: 10))
+                            .tint(.white)
+                    }
+                    Button {
+                        dismiss()
+                    } label: {
+                        Text("적당히")
+                            .padding()
+    //                        .frame(maxWidth: .infinity)
+                            .background(.green)
+                            .clipShape(RoundedRectangle(cornerRadius: 10))
+                            .tint(.white)
+                    }
+                    Button {
+                        dismiss()
+                    } label: {
+                        Text("격하게")
+                            .padding()
+    //                        .frame(maxWidth: .infinity)
+                            .background(.green)
+                            .clipShape(RoundedRectangle(cornerRadius: 10))
+                            .tint(.white)
+                    }
+                }
+
+
+            } header: {
+                Text("운동 강도")
+            }
+        }
+        Button {
+            dismiss()
+        } label: {
+            Text("Done")
+                .padding()
+                .frame(maxWidth: .infinity)
+                .background(.green)
+                .clipShape(RoundedRectangle(cornerRadius: 10))
+                .tint(.white)
+        }
+        .padding()
+
+
 
     }
 }
