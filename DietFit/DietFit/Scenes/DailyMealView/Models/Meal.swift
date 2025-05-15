46aef565d61a19b9f2bca7c793adc9aee47c65a8
박동언 <dongeon8943@gmail.com>
2025-05-15 09:56:46 +0900

    SwiftData 모델 수정

---
+++ DietFit/DietFit/Scenes/DailyMealView/Models/Meal.swift
@@ -0,0 +1,20 @@
+//
+//  Meal.swift
+//  DietFit
+//
+//  Created by 박동언 on 5/15/25.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Meal {
+    var type: MealType
+    var foods: [Food] = []
+
+    init(type: MealType, foods: [Food] = []) {
+        self.type = type
+        self.foods = foods
+    }
+}
---
+++ DietFit/DietFit/Scenes/DailyMealView/Models/MealRecord.swift
@@ -0,0 +1,22 @@
+//
+//  MealRecord.swift
+//  DietFit
+//
+//  Created by 박동언 on 5/15/25.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class MealRecord {
+    var date: Date
+    var meals: [Meal] = []
+    var memo: String?
+
+    init(date: Date = Date().startOfDay, meals: [Meal], memo: String? = nil) {
+        self.date = date
+        self.meals = meals
+        self.memo = memo
+    }
+}
