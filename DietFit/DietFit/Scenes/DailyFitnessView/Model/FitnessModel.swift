de1b3514863d9a249d398fde57944d5f04ceadcd
Heejung Yang <wolla39@gmail.com>
2025-05-15 09:42:41 +0900

    FitnessModel date 변수 구체적으로 변경

--- DietFit/DietFit/Scenes/DailyFitnessView/Model/FitnessModel.swift
+++ DietFit/DietFit/Scenes/DailyFitnessView/Model/FitnessModel.swift
@@ -11,14 +11,14 @@ import Foundation
 @Model
 final class FitnessModel: Identifiable, Hashable  {
     var id = UUID()
-    var date: Date
+    var insertDate: Date
     var area: String
     var exercise: String
     var duration: Int
     var intensity: Int
 
-    init(date: Date, area: String, exercise: String, duration: Int, intensity: Int) {
-        self.date = date
+    init(insertDate: Date = .now, area: String, exercise: String, duration: Int, intensity: Int) {
+        self.insertDate = insertDate
         self.area = area
         self.exercise = exercise
         self.duration = duration
