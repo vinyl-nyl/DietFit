52cc74fbba8cfcc8482398daf187d2c972468b9e
박동언 <dongeon8943@gmail.com>
2025-05-15 09:50:53 +0900

    DatePickerButton -> DatePickButtonView로 이름 변경

---
+++ DietFit/DietFit/Shared/DatePickerButtonView.swift
@@ -0,0 +1,42 @@
+//
+//  DatePickerButton.swift
+//  DietFit
+//
+//  Created by 박동언 on 5/14/25.
+//
+
+import SwiftUI
+
+struct DatePickerButtonView: View {
+    @Environment(\.horizontalSizeClass) private var horizontalSizeClass
+
+    @ObservedObject var mealVM: DailyMealViewModel
+
+    @State private var presentCalendar = false
+
+    var body: some View {
+        Button {
+            presentCalendar = true
+        } label: {
+            Text(dateFormat(mealVM.selectedDate))
+                .font(.title3)
+                .bold()
+            Image(systemName: "arrowtriangle.down.fill")
+        }
+        .buttonStyle(.plain)
+        .padding(.horizontal)
+        .sheet(isPresented: $presentCalendar) {
+            VStack {
+                DatePicker("SelecteDate", selection: $mealVM.selectedDate, displayedComponents: [.date])
+                    .datePickerStyle(.graphical)
+                    .tint(Color.buttonPrimary)
+                    .onChange(of: mealVM.selectedDate) { old, new in
+                        mealVM.days = mealVM.generateDays(new)
+                        presentCalendar = false
+                    }
+            }
+            .presentationDetents(horizontalSizeClass == .compact ? [.fraction(0.6)] : [.large])
+            .presentationDragIndicator(.visible)
+        }
+    }
+}
